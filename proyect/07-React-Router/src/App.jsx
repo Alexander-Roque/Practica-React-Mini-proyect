0c84490151f2d042bee92a1f3abe0ea253c492c0
lex <josalexander25@gmail.com>
2026-02-26 10:51:11 -0500

    Hacer-un-i18n

--- proyect/07-React-Router/src/App.jsx
+++ proyect/07-React-Router/src/App.jsx
@@ -1,14 +1,19 @@
 import * as React from "react"
 import { lazy, Suspense } from "react";
-import HomePage from "./pages/home";
+// import HomePage from "./pages/home";
 import { Router } from "./Router";
 import Error404 from "./pages/Error";
 import SearchDinamy from "./pages/SearchDinamy";
 import Route from "./Route";
 
 const LazyAboutPage = lazy(()=>import('./pages/about.jsx'))
+const LazyHome = lazy(()=>import('./pages/home.jsx'))
 
 const appRoutes = [
+  {
+    path: '/:lang/about',
+    Component: LazyAboutPage
+  },
   {
     path: '/search/:query',
     Component: SearchDinamy
@@ -21,7 +26,7 @@ function App() {
     <main>
       <Suspense fallback={<div>Loading...</div>}>
       <Router routes={appRoutes} defaultComponent={Error404}>
-        <Route path='/' Component={HomePage} />
+        <Route path='/' Component={LazyHome} />
         <Route path='/about' Component={LazyAboutPage} />
       </Router>
       </Suspense>
--- proyect/07-React-Router/src/pages/about.jsx
+++ proyect/07-React-Router/src/pages/about.jsx
@@ -1,15 +1,33 @@
 import { Link } from "../Link"
 
+const i18n = {
+  es:{
+    title: 'Sobre nosotros',
+    button: 'Ir la home',
+    description: '¡Hola! Me llamo Alexander Roque y estoy creando un clon de React Router'
+  },
+  en: {
+    title: 'About us',
+    button: 'go to Home',
+    description: '¡HI! My name is Alexander Roque and I am creatin a clone of React Router'
+  }
+}
 
-export default function About(){
+const useI18n = (lang)=> {
+  return i18n[lang] || i18n.en
+}
+
+export default function About({routeParams}){
+  const i18n = useI18n(routeParams.lang ?? 'es')
+
   return(
     <>
-    <h1>About</h1>
+    <h1>{i18n.title}</h1>
     <div>
       <img src='../public/Cori.png' alt='Foto del rey Pixie'/>
-    <p>Hola soy Alex y estoy creando una ruta para React Router</p>
+    <p>{i18n.description}</p>
     </div>
-    <Link to={'/'}>Ir a home</Link>
+    <Link to={'/'}>{i18n.button}</Link>
     </>
   )
 }
